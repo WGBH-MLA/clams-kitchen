117b5ceea9be3e0fcad48023f8b3368258b84769
owencking <92190360+owencking@users.noreply.github.com>
2025-01-14 12:31:39 -0500

    removed visaid_builder code, which is now in its own repo

--- visaid_builder/visaid_enhance.js
+++
@@ -1,9 +0,0 @@
-// 
-// A file named like this one can be used to enhance or alter an existing
-// visaid (for visaid version >= 1.20).
-// Add to this file to use JavaScript to enhance or transform a visaid
-
-
-// Add indication that enhancement has occurred
-el = document.getElementById('enhance-indicator');
-el.innerHTML = "<br>*";
